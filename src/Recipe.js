eb0f38ac20629410075825c05bf25d789d8212e1
Harsha Javle <hjavle@hotmail.com>
2020-12-11 01:22:59 -0500

    added title style and rounded calories

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import style from './recipe.module.css';
 
 const Recipe = ({title,calories,image,ingredients}) => {
     return (
-        <div>
-            <h1>{title}</h1>
-            <ol>
+        <div className = {style.recipe}>
+            <h1 className= {style.title}>{title}</h1>
+            <ol className={style.text}>
                 {ingredients.map(ingredient => (
-                    <li>{ingredient.text}</li>
+                    <li className={style.text}>{ingredient.text}</li>
                 ))}
             </ol>
-            <p>{calories}</p>
-            <img src={image} alt=""/>
+            <p className={style.calories}>Total Calories : {Math.round(calories)}</p>
+            <img className= {style.image} src={image} alt=""/>
         </div>
     );
 };
